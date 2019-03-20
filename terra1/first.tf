9b10ae2832dc69b38ccb2e591ee90a37b85c3b0d
Ubuntu <ubuntu@ip-172-31-44-194.ec2.internal>
2019-03-20 17:40:30 +0000

    stuff

--- terra1/first.tf
+++ terra1/first.tf
@@ -19,22 +19,3 @@ resource "aws_instance" "instance" {
     }
  }
 }
-data "template_file" "dev_hosts" {
-  template = "${file("dev_hosts.cfg")}"
-  depends_on = [
-    "aws_instance.instance"
-  ]
-  vars {
-    api_public = "${aws_instance.instance.public_ip}"
-    host = "host01"
-  }
-}
-
-resource "null_resource" "dev-hosts" {
-  triggers {
-    template_rendered = "${data.template_file.dev_hosts.rendered}"
-  }
-  provisioner "local-exec" {
-    command = "echo '${data.template_file.dev_hosts.rendered}' > /home/ubuntu/ansible_jk/inventory/hosts"
-  }
-}
